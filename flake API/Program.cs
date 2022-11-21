3182d09b16523f4adcc0a1a6464cc51ad35803be
nayem <nayem@SNIFFER-JR>
2022-11-21 23:34:41 +0600

    Configured CORS policy

--- flake API/Program.cs
+++ flake API/Program.cs
@@ -15,6 +15,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure CORS policy
+builder.Services.AddCors(p => p.AddPolicy("corspolicy",build => { 
+    build.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("corspolicy");
 
 app.UseAuthorization();
 
