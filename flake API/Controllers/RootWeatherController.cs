5a7426cd4e7bc2b56031d0cf4f5a44a8bc309f86
nayem <nayem@SNIFFER-JR>
2022-11-21 11:20:27 +0600

    Injected customtom logger Serilog.ASPNETCore

--- flake API/Controllers/RootWeatherController.cs
+++ flake API/Controllers/RootWeatherController.cs
@@ -11,6 +11,12 @@ namespace flake_API.Controllers
 
     public class RootWeatherController : ControllerBase
     {
+        private readonly ILogger<RootWeatherController> _logger;
+
+        public RootWeatherController(ILogger<RootWeatherController> logger)
+        {
+            _logger = logger;
+        }
 
         // <<API endpoints>>
 
@@ -24,28 +30,48 @@ namespace flake_API.Controllers
         // Get LATEST weather data for a specific location
         [HttpGet("{location}", Name = "GetCurrentData")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<WeatherDTOModel> GetCurrentWeatherData(string location)
         {
             location = location.ToLower();
             if (!TemporaryDataStorage.WeatherData.ContainsKey(location))
+            {
+                _logger.LogWarning("____Attempted invalid location!");
                 return NotFound();
+            }
+            if (TemporaryDataStorage.WeatherData[location].Count == 0)
+            {
+                _logger.LogError("____Data repository currently empty!");
+                return NoContent();
+            }
+            _logger.LogInformation($"____Getting latest data from {location}.");
             return Ok(TemporaryDataStorage.WeatherData[location][TemporaryDataStorage.WeatherData[location].Count - 1]);
         }
 
 
         // Get LATEST weather data upto a specific count
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpGet("{location}/{count:int}",Name = "GetCurrentDataUptoCount")]
         public ActionResult<IEnumerable<WeatherDTOModel>> GetCurrentWeatherDataUptoCount(string location,int count) {
             location = location.ToLower();
             if (!TemporaryDataStorage.WeatherData.ContainsKey(location))
+            {
+                _logger.LogWarning("____Attempted invalid location!");
                 return NotFound();
+            }
+            if (TemporaryDataStorage.WeatherData[location].Count == 0)
+            {
+                _logger.LogError("____Data repository currently empty!");
+                return NoContent();
+            }
             if (count <= 0) return BadRequest($"Count:{count} should be greater than zero!");
             var x = TemporaryDataStorage.WeatherData[location].Count;
             count = count > x ? x % count : count;
+            _logger.LogInformation($"____Getting latest data from {location} upto {count}.");
             return Ok(TemporaryDataStorage.WeatherData[location]
                 .ToList<WeatherDTOModel>().GetRange(x - count,count));
         }
@@ -53,15 +79,24 @@ namespace flake_API.Controllers
 
         [HttpGet("{location}/{date:DateTime}",Name = "GetDataFromDate")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<IEnumerable<WeatherDTOModel>> GetWeatherDataFromDate(string location,DateTime date)
         {
             location = location.ToLower();
             if (!TemporaryDataStorage.WeatherData.ContainsKey(location))
+            {
+                _logger.LogWarning("____Attempted invalid location!");
                 return NotFound();
-            if (date > DateTime.Today)
-                return BadRequest("Invalid date arguement!");
+            }
+            if (TemporaryDataStorage.WeatherData[location].Count == 0)
+            {
+                _logger.LogError("____Data repository currently empty!");
+                return NoContent();
+            }
+            if (date > DateTime.Today) return BadRequest("Invalid date arguement!");
+            _logger.LogInformation($"____Getting data from {location} of the {date}.");
             return Ok(TemporaryDataStorage.WeatherData[location]
                 .ToList<WeatherDTOModel>().Where(x => x.Time >= date || x.Time < date.AddDays(1)));
         }
@@ -70,12 +105,22 @@ namespace flake_API.Controllers
         // Get ALL weather data for a specific location
         [HttpGet("all/{location}",Name = "GetAllData")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<IEnumerable<WeatherDTOModel>> GetAllWeatherData(string location)
         {
             location = location.ToLower();
             if (!TemporaryDataStorage.WeatherData.ContainsKey(location))
+            {
+                _logger.LogWarning("____Attempted invalid location!");
                 return NotFound();
+            }
+            if (TemporaryDataStorage.WeatherData[location].Count == 0)
+            {
+                _logger.LogError("____Data repository currently empty!");
+                return NoContent();
+            }
+            _logger.LogInformation($"____Getting today's data from {location}.");
             return Ok(TemporaryDataStorage.WeatherData[location]
                 .ToList<WeatherDTOModel>().Where<WeatherDTOModel>(x => x.Time >= DateTime.Today));
         }
@@ -87,12 +132,37 @@ namespace flake_API.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<WeatherDTOModel> StoreNewData([FromBody]WeatherDTOModel dataModel,string location)
         {
+            location = location.ToLower();
             if (!TemporaryDataStorage.WeatherData.ContainsKey(location))
-                return BadRequest($"{location} is invalid!");
-            if (dataModel is null)
-                return BadRequest();
+            {
+                _logger.LogWarning("____Attempted invalid location!");
+                return BadRequest($"{location} is invalid location!");
+            }
+            if (dataModel is null) return BadRequest(dataModel);
             TemporaryDataStorage.WeatherData[location].Add(dataModel);
+            _logger.LogInformation($"____New entry at {location}.");
             return CreatedAtRoute("GetCurrentData", new { location = location} ,dataModel);
+        }
+
+
+        [HttpDelete("{location}",Name = "DeleteLastEntry")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult DeleteWeatherData(string location) {
+            location = location.ToLower();
+            if (!TemporaryDataStorage.WeatherData.ContainsKey(location))
+            {
+                _logger.LogWarning("____Attempted invalid location!");
+                return NotFound($"{location} is invalid location!");
+            }
+            if (TemporaryDataStorage.WeatherData[location].Count == 0)
+            {
+                _logger.LogError("____Data repository currently empty!");
+                return NoContent();
+            }
+            TemporaryDataStorage.WeatherData[location].RemoveAt(TemporaryDataStorage.WeatherData[location].Count - 1);
+            _logger.LogWarning($"____Last entry from {location} was deleted!");
+            return NoContent();
         }
 
         // <<API endpoints>>
--- flake API/Program.cs
+++ flake API/Program.cs
@@ -1,6 +1,14 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+Log.Logger = new LoggerConfiguration().MinimumLevel.Information()
+    .WriteTo.File("Logs/apilogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
